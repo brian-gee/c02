3ae4acd422434fd7e3bd12236e5069c053b98207
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-12-21 19:01:10 -0500

    Initial Commit

---
+++ c02/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  c02
+//
+//  Created by brian on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ c02/c02App.swift
@@ -0,0 +1,17 @@
+//
+//  c02App.swift
+//  c02
+//
+//  Created by brian on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct c02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
